ad71c43ce5c9ec5905e76b759b248a2474c98fd7
Adnan <adbak.dev@gmail.com>
2023-08-23 01:53:41 +0200

    state managing dice

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -8,8 +9,6 @@ class GradientContainer extends StatelessWidget {
 
   final Color colorA;
   final Color colorB;
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -24,26 +23,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 30),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
