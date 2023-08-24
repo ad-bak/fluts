bd19a7e7348af07869a3b3c61272a971c8a8e35a
Adnan <adbak.dev@gmail.com>
2023-08-24 02:21:43 +0200

    change screen

---
+++ #02 quiz/lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:basics/questions_screen.dart';
+import 'package:basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 255, 66, 0), // Orange-ish
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
--- #02 quiz/lib/start_screen.dart
+++ #02 quiz/lib/start_screen.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text("Start Screen"),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(137, 255, 255, 255),
+          ),
+          const SizedBox(height: 100),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 50),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              side: const BorderSide(color: Colors.white),
+            ),
+            icon: const Icon(Icons.keyboard_double_arrow_right),
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
