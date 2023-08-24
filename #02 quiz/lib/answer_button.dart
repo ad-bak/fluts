3487ee087acd7e47fea051a412bb2eb64b5b8954
Adnan <adbak.dev@gmail.com>
2023-08-24 20:07:38 +0200

    new project

--- #02 quiz/lib/answer_button.dart
+++ #02 quiz/lib/answer_button.dart
@@ -15,7 +15,7 @@ class AnswerButton extends StatelessWidget {
         onPressed: onTap,
         style: ElevatedButton.styleFrom(
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-          backgroundColor: Color.fromARGB(255, 130, 58, 180),
+          backgroundColor: const Color.fromARGB(255, 130, 58, 180),
           foregroundColor: Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20),
