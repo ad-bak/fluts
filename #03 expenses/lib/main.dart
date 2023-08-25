783b8381cb34101bd3667aed1c14e3785cafa404
Adnan <adbak.dev@gmail.com>
2023-08-25 04:07:21 +0200

    expense card

--- #03 expenses/lib/main.dart
+++ #03 expenses/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/expenses.dart';
+import 'package:app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ #03 expenses/lib/widgets/expenses.dart
@@ -0,0 +1,38 @@
+import 'package:app/widgets/expenses_list/expenses_list.dart';
+import 'package:app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        const Text('Expenses'),
+        Expanded(child: ExpensesList(expenses: _registeredExpenses))
+      ],
+    ));
+  }
+}
