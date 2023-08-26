c261f60ff6fbe4dbfdb5ccefd0abf3b56e7b9c24
Adnan <adbak.dev@gmail.com>
2023-08-26 05:50:44 +0200

    layout

--- #03 expenses/lib/widgets/new_expense.dart
+++ #03 expenses/lib/widgets/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:app/models/expense.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,23 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentedDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -38,13 +56,49 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ),
             const SizedBox(height: 16),
-            TextField(
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                prefixText: '\$',
-                label: Text('Amount'),
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      prefixText: '\$',
+                      label: Text('Amount'),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                TextButton.icon(
+                    onPressed: _presentedDatePicker,
+                    icon: const Icon(Icons.calendar_month),
+                    label: Text(_selectedDate == null
+                        ? 'No date selected'
+                        : formatter.format(_selectedDate!)))
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                DropdownButton(
+                    value: _selectedCategory,
+                    items: Category.values
+                        .map(
+                          (category) => DropdownMenuItem(
+                            value: category,
+                            child: Text(
+                              category.name.toUpperCase(),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        if (value == null) return;
+                        _selectedCategory = value;
+                      });
+                    }),
+              ],
             ),
             Row(
               children: [
@@ -63,7 +117,7 @@ class _NewExpenseState extends State<NewExpense> {
                   child: const Text('Save Expense'),
                 )
               ],
-            ),
+            )
           ],
         ));
   }
