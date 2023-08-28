0181bf1b81985489a194fa95e5cd486aa1bad481
Adnan <adbak.dev@gmail.com>
2023-08-28 23:09:19 +0200

    meals screen

--- #04 meals/lib/main.dart
+++ #04 meals/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:meals/screens/categories.dart';
 
 final theme = ThemeData(
