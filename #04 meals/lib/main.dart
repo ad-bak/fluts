f3c3ea980937eefc113aaa22cd4efcc83511eeb1
Adnan <adbak.dev@gmail.com>
2023-08-30 00:06:30 +0200

    card

--- #04 meals/lib/main.dart
+++ #04 meals/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,7 +8,7 @@ final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: Color.fromARGB(255, 251, 0, 0),
+    seedColor: const Color.fromARGB(255, 251, 0, 0),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
