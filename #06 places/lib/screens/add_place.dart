3b7527eb6d0ae21bf59451e472102b4a4f9d70c5
Adnan <adbak.dev@gmail.com>
2023-09-03 23:14:21 +0200

    taking image

--- #06 places/lib/screens/add_place.dart
+++ #06 places/lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:places/providers/user_places.dart';
+import 'package:places/widgets/image_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -62,6 +63,8 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 16),
+            const ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
