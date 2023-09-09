86dd930c3c70bdfb9fabc8ed9199fd5e8c30fbcc
Adnan <adbak.dev@gmail.com>
2023-09-09 16:17:01 +0200

    locaiton

--- #06 places/lib/screens/add_place.dart
+++ #06 places/lib/screens/add_place.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:places/providers/user_places.dart';
 import 'package:places/widgets/image_input.dart';
+import 'package:places/widgets/location_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -71,6 +72,8 @@ class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(height: 16),
+            LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
